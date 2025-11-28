d41eef95c517ed433037087d5cf99c17c8f3e07f
dazzes <dazzes7619@gmail.com>
2025-11-28 21:01:40 +0300

    DZ

---
+++ DZ/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public bool IsAvailable;
+
+        public Book()
+        {
+            Title = "Неизвестно";
+            Author = "Неизвестен";
+            Year = 0;
+            IsAvailable = false;
+        }
+
+        public Book(string title, string author, int year, bool isAvailable)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            IsAvailable = isAvailable;
+        }
+
+        public void PrintInfo()
+        {
+            string availableText;
+            if (IsAvailable)
+            {
+                availableText = "Да";
+            }
+            else
+            {
+                availableText = "Нет";
+            }
+            Console.WriteLine($"Название: \"{Title}\", Автор: {Author}, Год: {Year}, Доступна: {availableText}");
+        }
+    }
+
+}
---
+++ DZ/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.PrintInfo();
+
+            Book book2 = new Book("Война и мир", "Толстой", 1869, true);
+            book2.PrintInfo();
+        }
+    }
+}
